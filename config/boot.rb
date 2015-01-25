8fe4f548a8293c7118b1b1feb9ad1b4c8ce523df
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-01-25 12:51:45 +0100

    Load config/initializers/* files.

--- config/boot.rb
+++ config/boot.rb
@@ -6,6 +6,14 @@ Bundler.require(:default, ENV.fetch('RACK_ENV')) if defined? Bundler
 # Load .env variables
 Dotenv.load
 
+# Require initializers
+initializer_files = Dir[File.expand_path('../../config/initializers/**/*.rb', __FILE__)]
+initializer_files.each do |file|
+  dirname = File.dirname(file)
+  file_basename = File.basename(file, File.extname(file))
+  require "#{dirname}/#{file_basename}"
+end
+
 # Configure Mongoid
 database_config = File.expand_path('../../config/mongoid.yml', __FILE__)
 Mongoid.load!(database_config, ENV.fetch('RACK_ENV'))
