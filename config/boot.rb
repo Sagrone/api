29368bb5177539c266162482615b164aef9918f5
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-01-25 13:03:46 +0100

    First thing require dotenv vars.

--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,11 @@
+# Load .env variables
+require 'dotenv'
+Dotenv.load
+
 # Bundler setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV.fetch('BUNDLE_GEMFILE'))
 Bundler.require(:default, ENV.fetch('RACK_ENV')) if defined? Bundler
-
-# Load .env variables
-Dotenv.load
 
 # Require initializers
 initializer_files = Dir[File.expand_path('../../config/initializers/**/*.rb', __FILE__)]
