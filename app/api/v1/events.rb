6a766720e5c3966b2bdf3ddd78c55d9a2fae1f6f
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-22 18:09:16 +0100

    GET v1/events returns all events.

--- app/api/v1/events.rb
+++ app/api/v1/events.rb
@@ -1,11 +1,18 @@
+require 'models/event'
+require 'api/entities/event'
+
 module API
   module V1
     class Events < Grape::API
       include API::V1::Defaults
 
+      desc 'Return all events', {
+        params: API::Entities::Event.documentation
+      }
       namespace :events do
         get nil do
-          { ok: true}
+          events = Models::Event.all
+          present :events, events, with: API::Entities::Event
         end
       end
     end
--- spec/api/v1/events_spec.rb
+++ spec/api/v1/events_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'models/event'
 
 RSpec.describe API::V1::Events do
   it 'should be defined' do
@@ -9,6 +10,34 @@ RSpec.describe API::V1::Events do
     it 'should have status 200' do
       get 'v1/events'
       expect_status 200
+    end
+
+    describe 'with three events present' do\
+      before do
+        3.times do |num|
+          Models::Event.create!({
+            title: "Title #{num}",
+            description: "Description #{num}"
+          })
+        end
+        get 'v1/events'
+      end
+
+      it 'should return three events as json' do
+        expect_json_sizes(events: 3)
+      end
+
+      it 'should have fields with correct types' do
+        expect_json_types('events.*', {title: :string, description: :string})
+      end
+
+      it 'first event returned has correct values' do
+        expect_json('events.0', {title: 'Title 0', description: 'Description 0'})
+      end
+
+      it 'last event returned has correct values' do
+        expect_json('events.2', {title: 'Title 2', description: 'Description 2'})
+      end
     end
   end
 end
