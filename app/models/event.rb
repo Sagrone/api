e1f6b13f261bb30f64c8f8488af028324db15f09
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-22 16:47:55 +0100

    Event model has title field present of type String.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,9 @@ module Models
   class Event
     include Mongoid::Document
     include Mongoid::Timestamps
+
+    field :title, type: String
+
+    validates :title, presence: true
   end
 end
--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 require 'models/event'
 
-RSpec.describe Models::Event do
+RSpec.describe Models::Event, type: :model do
   it 'should be defined' do
     expect(described_class).to_not be_nil
   end
 
   it { is_expected.to be_mongoid_document }
   it { is_expected.to be_timestamped_document }
+
+  it { is_expected.to have_field(:title).of_type(String) }
+  it { is_expected.to validate_presence_of(:title) }
 end
