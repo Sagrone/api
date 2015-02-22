dc4b1efe33493865dbab521eed46192000e50d9d
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-22 17:22:51 +0100

    GET /v1/events returns 200 status.

--- app/api/v1/events.rb
+++ app/api/v1/events.rb
@@ -1,6 +1,12 @@
 module API
   module V1
     class Events < Grape::API
+      namespace :events do
+
+        get nil do
+          { ok: true}
+        end
+      end
     end
   end
 end
--- spec/api/v1/events_spec.rb
+++ spec/api/v1/events_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe API::V1::Events do
   it 'should be defined' do
     expect(described_class).to_not be_nil
   end
+
+  describe 'GET /v1/events' do
+    it 'should have status 200' do
+      get 'v1/events'
+      expect_status 200
+    end
+  end
 end
