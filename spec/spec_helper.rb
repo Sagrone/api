c5c57f12cdef5cd7d2e8cc59e61106a9d29f805a
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-22 15:45:28 +0100

    Update rspec config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,17 +8,25 @@ require 'rspec'
 require_relative '../config/boot'
 
 Airborne.configure do |config|
-  config.rack_app = API
+  config.rack_app = API::Base
 end
 
 RSpec.configure do |config|
-  config.mock_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
   end
 
-  config.full_backtrace = false
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
 end
