f7f8a87b71f9321b12827b01f73c775ec2488402
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-22 16:49:24 +0100

    Describe groups.

--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe Models::Event, type: :model do
     expect(described_class).to_not be_nil
   end
 
-  it { is_expected.to be_mongoid_document }
-  it { is_expected.to be_timestamped_document }
+  describe 'is a document' do
+    it { is_expected.to be_mongoid_document }
+    it { is_expected.to be_timestamped_document }
+  end
 
-  it { is_expected.to have_field(:title).of_type(String) }
-  it { is_expected.to validate_presence_of(:title) }
+  describe 'has fields' do
+    it { is_expected.to have_field(:title).of_type(String) }
+    it { is_expected.to validate_presence_of(:title) }
+  end
 end
