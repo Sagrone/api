81e9089d28016d9ef333a74023d5cb9ef8499500
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-03 16:44:36 +0200

    Add Event#coordinates array field with 2d index.

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,8 +8,11 @@ class Event
 
   field :title, type: String
   field :description, type: String
+  field :coordinates, type: Array
 
   embeds_one :organizer, class_name: 'Event::Organizer'
+
+  index({ coordinates: '2d' }, { min: -180, max: 180 })
 
   validates :title, presence: true
   validates :description, presence: true
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,9 +16,15 @@ RSpec.describe Event, type: :model do
 
     it { is_expected.to have_field(:description).of_type(String) }
     it { is_expected.to validate_presence_of(:description) }
+
+    it { is_expected.to have_field(:coordinates).of_type(Array) }
   end
 
   describe 'associations' do
     it { is_expected.to embed_one(:organizer) }
+  end
+
+  describe 'indexes' do
+    it { is_expected.to have_index_for(coordinates: '2d').with_options(min: -180, max: 180) }
   end
 end
