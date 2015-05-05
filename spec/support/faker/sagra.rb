838afaacf351a026dce1df36e8ca70bbeff5c4f3
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-05 18:22:47 +0200

    Add custom Faker::Sagra support in test.

---
+++ spec/support/faker/sagra.rb
@@ -0,0 +1,127 @@
+require 'faker'
+
+module Faker
+  module Sagra
+    def self.title
+      num_edition = (1..20).to_a.sample
+      "Sagra della #{sagre_examples.sample} #{num_edition} edizione"
+    end
+
+    def self.description
+      Faker::Lorem.paragraph(10)
+    end
+
+    def self.organizer
+      {
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.phone_number,
+        website: Faker::Internet.url
+      }
+    end
+
+    def self.latitude
+      Faker::Address.latitude.to_f
+    end
+
+    def self.longitude
+      Faker::Address.longitude.to_f
+    end
+
+    def self.coordinates
+      [latitude, longitude]
+    end
+
+    def self.date
+      {
+        start_at: Faker::Date.between(2.days.ago, 5.days.from_now),
+        end_at: Faker::Date.between(5.days.from_now, 10.days.from_now),
+      }
+    end
+
+    def self.full_address
+      "#{street_name}, #{street_number}, #{city}, #{region}"
+    end
+
+    def self.street_name
+      Faker::Address.street_name
+    end
+
+    def self.street_number
+      Faker::Number.number(2)
+    end
+
+    def self.city
+      Faker::Address.city
+    end
+
+    def self.zip
+      Faker::Address.zip
+    end
+
+    def self.region
+      regions_examples.sample
+    end
+
+    def self.country
+      'Italy'
+    end
+
+    def self.address
+      lat = latitude
+      lon = longitude
+
+      {
+        latitude: lat,
+        longitude: lon,
+        street: street_name,
+        street_number: street_number,
+        city: city,
+        zip: zip,
+        region: region,
+        country: country,
+        google_map_url: "http://maps.google.com/maps?q=#{lat},#{lon}"
+      }
+    end
+
+    def self.tags
+      tags_examples.shuffle.take(3)
+    end
+
+    def self.origin
+      {
+        name: 'example.com',
+        code_name: 'example.com',
+        website: Faker::Internet.url('example.com', '/'),
+        page_url: Faker::Internet.url('example.com')
+      }
+    end
+
+    def self.images(count = 5)
+      count.times.inject([]) do |result|
+        image = {
+          title: Faker::Name.title,
+          url: 'http://placehold.it/270x160',
+          width: '270',
+          height: '160'
+        }
+
+        result.push(image)
+      end
+    end
+
+    private
+
+    def self.sagre_examples
+      @_sagre_examples ||= %w(patata uva polenta seppia castagna noce mela)
+    end
+
+    def self.regions_examples
+      @_regions_examples ||= %w(Piemonte Lombardia Veneto Liguria)
+    end
+
+    def self.tags_examples
+      @_tags_examples ||= %w(funghi costine musica tombola sorpresa beneficenza)
+    end
+  end
+end
